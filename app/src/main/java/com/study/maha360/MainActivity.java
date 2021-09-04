dea353c2c5765379c75dd2f9c37085552af29c16
phonix007 <telegramtutorial007@gmail.com>
2021-09-04 13:46:36 +0530

    404

---
+++ app/src/main/java/com/study/maha360/MainActivity.java
@@ -0,0 +1,208 @@
+package com.study.maha360;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.webkit.CookieManager;
+import android.webkit.DownloadListener;
+import android.webkit.URLUtil;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import java.io.File;
+import java.net.URL;
+import java.util.Calendar;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    String websiteURL = "https://drive.google.com/drive/u/0/folders/0Bz9C0ysJZ7PnMGZKeWcybUpXWGM?resourcekey=0-S2yaWXvAG7ObM_GC8LRNTQ"; // sets web url
+    private WebView webview;
+    private ProgressBar progressBar;
+
+   
+
+    private long backPressedTime;
+    private Toast backToast;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progress_bar);
+        progressBar.setMax(100);
+
+
+        if (!CheckNetwork.isInternetAvailable(this)) //returns true if internet available
+        {
+            //if there is no internet do this
+            setContentView(R.layout.activity_main);
+            //Toast.makeText(this,"No Internet Connection, Chris",Toast.LENGTH_LONG).show();
+
+            new AlertDialog.Builder(this) //alert the person knowing they are about to close
+                    .setTitle("No internet connection available")
+                    .setMessage("Please Check you're Mobile data or Wifi network.")
+                    .setPositiveButton("Exit", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    //.setNegativeButton("No", null)
+                    .show();
+
+        } else {
+            //Webview stuff
+            webview = findViewById(R.id.webView);
+            webview.getSettings().setJavaScriptEnabled(true);
+            webview.getSettings().setDomStorageEnabled(true);
+            webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+            webview.loadUrl(websiteURL);
+            progressBar.setProgress(0);
+            webview.setWebViewClient(new WebViewClientDemo());
+
+            webview.setWebChromeClient(new WebChromeClient(){
+
+                @Override
+                public void onProgressChanged(WebView view, int newProgress) {
+                    progressBar.setProgress(newProgress);
+                    if (newProgress == 100)
+                        progressBar.setVisibility(View.INVISIBLE);
+                    else
+                        progressBar.setVisibility(View.VISIBLE);
+                    super.onProgressChanged(view, newProgress);
+                }
+            });
+
+
+
+
+        }
+
+        //Swipe to refresh functionality
+
+
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+            if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+
+                Log.d("permission","permission denied to WRITE_EXTERNAL_STORAGE - requesting it");
+                String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                requestPermissions(permissions,1);
+            }
+
+
+        }
+
+//handle downloading
+        try {
+        webview.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimeType, long contentLength) {
+
+
+                String currentTime = java.text.DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime());
+
+                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+                request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI | DownloadManager.Request.NETWORK_MOBILE);
+                request.setMimeType(mimeType);
+                String cookies = CookieManager.getInstance().getCookie(url);
+                String fileName = URLUtil.guessFileName(url, contentDisposition, mimeType);
+                request.addRequestHeader("cookie",cookies);
+                request.addRequestHeader("User-Agent",userAgent);
+                request.setDescription("Saved On Storage/Downloads/");
+                request.setTitle( "Maha_360_App_" + currentTime + URLUtil.guessFileName(url,contentDisposition,mimeType));
+                request.allowScanningByMediaScanner();
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "Maha_360_App_"+ currentTime + URLUtil.guessFileName(url, contentDisposition, mimeType));
+                DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                dm.enqueue(request);
+                Toast.makeText(getApplicationContext(),"Downloading File...",Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        }catch (Exception e){
+            Toast.makeText(MainActivity.this, "Something Went Wrong! Please Check Your Internet Connection...", Toast.LENGTH_LONG).show();
+            Toast.makeText(MainActivity.this, "Something Went Wrong! Please Check Your Internet Connection...", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+
+    private class WebViewClientDemo extends WebViewClient {
+        @Override
+        //Keep webview in app when clicking links
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+        }
+    }
+
+    //set back button functionality
+    @Override
+    public void onBackPressed() { //if user presses the back button do this
+        if (webview.isFocused() && webview.canGoBack()) { //check if in webview and the user can go back
+            webview.goBack(); //go back in webview
+        } else { //do this if the webview cannot go back any further
+
+            if (backPressedTime + 2000 > System.currentTimeMillis()) {
+                backToast.cancel();
+                super.onBackPressed();
+                return;
+            } else {
+                backToast = Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT);
+                backToast.show();
+            }
+            backPressedTime = System.currentTimeMillis();
+        }
+
+    }
+}
+
+
+class CheckNetwork {
+
+    private static final String TAG = CheckNetwork.class.getSimpleName();
+
+    public static boolean isInternetAvailable(Context context) {
+        NetworkInfo info = (NetworkInfo) ((ConnectivityManager)
+                context.getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
+
+        if (info == null) {
+            Log.d(TAG, "no internet connection");
+            return false;
+        } else {
+            if (info.isConnected()) {
+                Log.d(TAG, " internet connection available...");
+                return true;
+            } else {
+                Log.d(TAG, " internet connection");
+                return true;
+            }
+
+        }
+    }
+}
